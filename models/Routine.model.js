7547b031dbcd428b519bc664b2a87dc8f7948103
Asier González <asiergontz8@gmail.com>
2023-05-04 19:10:58 +0200

    testing github

---
+++ models/Routine.model.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require("mongoose");
+
+const routineSchema = new Schema({
+  bodyPart: {
+    type: String,
+    required: true,
+  },
+  day: {
+    type: Date,
+    required: true,
+  },
+  exercises: [{
+    name: String,
+    repetitions: Number
+    }]
+  ,
+  length: {
+    type: String,
+    required: true,
+  },
+  difficulty: {
+    type: String,
+    enum: ["Beginner", "Intermediate", "Advanced"],
+    required: true,
+  },
+  comments: [String],
+  trainer: { type: Schema.Types.ObjectId, ref: "Trainer" },
+  user: { type: Schema.Types.ObjectId, ref: "User" },
+});
+
+const Routine = model("Routine", routineSchema);

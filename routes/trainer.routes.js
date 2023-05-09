6c57d724009bc1ebffaf6425d219e0eac4371ccf
Asier González <asiergontz8@gmail.com>
2023-05-09 13:17:56 +0200

    trainer routes preventivo

--- routes/trainer.routes.js
+++ routes/trainer.routes.js
@@ -74,7 +74,7 @@ router.get("/dashboard-trainer", (req, res, next) => {
     });
 
 
-//Client-details
+//CLIENT-DETAILS//
 
 router.get("/client-details/:id", (req, res) => {
     User.findById(req.params.id)
@@ -85,10 +85,134 @@ router.get("/client-details/:id", (req, res) => {
       .catch((error) => next(error));
   });
 
+  //delete routine//
+
+  router.post('/client-details/:id/delete', (req, res, next) => {
+    const { routineId } = req.params.id;
+    Routine.findByIdAndDelete(routineId)
+        .populate('routines')
+      .then(() => res.render('trainer/client-details/:id'))
+      .catch(error => next(error));
+  });
+
+
+//CLIENT-ROUTINE DETAILS//
+
+router.get("/routine-client/:id", (req, res) => {
+    Routine.findById(req.params.id)
+      .then((routineDetails) => {
+        res.render("trainer/routine-client", { routineDetails });
+      })
+      .catch((error) => next(error));
+  });
+
+  //edit routine//
+
+  router.get('/routine-client/:Id/edit', (req, res, next) => {
+    const { routineId } = req.params;
+   
+    Routine.findById(routineId)
+      .then(routineToEdit => {
+        res.render('trainer/routine-edit.hbs', { routine: routineToEdit });
+      })
+      
+      .catch(error => next(error));
+  });
+
+  router.post('/routine-client/:Id/edit', (req, res, next) => {
+    const {routineId} = req.params
+    const { bodyPart, day, exercise, length, difficulty } = req.body
+
+    Routine.findByIdAndUpdate (routineId, {bodyPart, day, exercise, length, difficulty}, {new:true})
+    .then( updatedRoutine => res.redirect('/routine-client/ ${updatedRoutine.id}'))
+    .catch((err) => next(err))
+  })
 
 
 
+ //CLIENT REGISTRATION
 
- // Create user//
+router.get("/client-registration", (req, res, next) => {
+    res.render("trainer/client-registration");
+  });
+  
+  router.post("/client-registration", (req, res, next) => {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      res.status(400).render("trainer/client-registration", {
+        errormessage:
+          "All fields are mandatory. Please provide all requested fields.",
+      });
+      return;
+    }
+    User.findOne({ email }).then((existingUser) => {
+      if (existingUser) {
+        res.status(400).render("trainer/client-registration", {
+          errormessage: "This email already exists. Please provide a new email.",
+        });
+        return;
+      }
+    });
+    bcryptjs
+      .genSalt(saltRounds)
+      .then((salt) => bcryptjs.hash(password, salt))
+      .then((hashedPassword) => {
+        return User.create({
+          name,
+          email,
+          password: hashedPassword,
+        });
+      })
+      .then(() => {
+        res.redirect("/");
+      })
+      .catch((err) => next(err));
+  });
+
+  //SIGN UP TRAINERS
+router.get("/signup-trainer", (req, res, next) => {
+    res.render("auth/signup-trainer");
+  });
+  
+  router.post("/signup-trainer", (req, res, next) => {
+    const { name, email, password, phoneNumber } = req.body;
+    if (!name || !email || !password || !phoneNumber) {
+      res.status(400).render("auth/signup-trainer", {
+        errormessage:
+          "All fields are mandatory. Please provide all requested fields.",
+      });
+      return;
+    }
+    Trainer.findOne({ email }).then((existingUser) => {
+      if (existingUser) {
+        res.status(400).render("auth/signup-trainer", {
+          errormessage: "This email already exists. Please provide a new email.",
+        });
+        return;
+      }
+    });
+    /*if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/.test(password)) {
+      res.render("auth/signup-trainer", {
+        errorMessage:
+          "Password needs to have at least 8 characters and must contain at least one number, one lowercase and one uppercase letter.",
+      });
+      return;
+    }*/
+    bcryptjs
+      .genSalt(saltRounds)
+      .then((salt) => bcryptjs.hash(password, salt))
+      .then((hashedPassword) => {
+        return Trainer.create({
+          name,
+          email,
+          password: hashedPassword,
+          phoneNumber,
+        });
+      })
+      .then(() => {
+        res.redirect("/");
+      })
+      .catch((err) => next(err));
+  });
 
  module.exports = router;
